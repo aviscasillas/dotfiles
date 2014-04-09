da1457bed6f345bafa226fb193bc308e5d18a797
Albert Viscasillas <aviscasillas@gmail.com>
2014-04-09 18:13:55 +0200

    Emacs editor for all

--- system/env.zsh
+++ system/env.zsh
@@ -1,8 +1,5 @@
 # Only set this if we haven't set $EDITOR up somewhere else previously.
 if [[ "$EDITOR" == "" ]] ; then
   # Use sublime for my editor.
-  export EDITOR='subl'
+  export EDITOR='emacs'
 fi
-
-# Set vi tips to edit text in command line
-set -o vi
