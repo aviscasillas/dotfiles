2d0628ff97be17a0b3f17a9025f1d126c8d9718e
Albert Viscasillas <aviscasillas@gmail.com>
2014-06-04 13:00:32 +0200

    Auto install of emacs auto-complete plugin

--- emacs/prelude.zsh
+++ emacs/prelude.zsh
@@ -1,3 +1,11 @@
 cp -f ~/.dotfiles/emacs/prelude-modules.el ~/.emacs.d/
 rm ~/.emacs.d/personal/*.el
 cp ~/.dotfiles/emacs/personal/*.el ~/.emacs.d/personal/
+
+# Install auto-complete plugin if doesn't exist
+if ! [ -f $HOME/.emacs.d/auto-complete.el ]
+then
+    git clone https://github.com/auto-complete/auto-complete.git $HOME/auto-complete
+    emacs -batch -l $HOME/auto-complete/etc/install.el $HOME/.emacs.d/
+    rm -rf $HOME/auto-complete
+fi
